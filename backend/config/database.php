d8ad61cec6d452d7bca1b4b1d5ca0a3075a2f1a2
sebastianmiracastro <smira44@misena.edu.co>
2025-07-06 20:48:45 -0500

    Feature: se crea clase para controlar las conexiones a base de datos, adicional de añade archivos .env para control de variables de entorno [20250706]

---
+++ backend/config/database.php
@@ -0,0 +1,36 @@
+<?php
+
+class Database
+{
+    private $host;
+    private $db_name;
+    private $username;
+    private $password;
+    private $port;
+    public $conn;
+
+    public function __construct()
+    {
+        $env = parse_ini_file(__DIR__ . '/../.env');
+        $this->host = $env['DB_HOST'];
+        $this->db_name = $env['DB_NAME'];
+        $this->username = $env['DB_USER'];
+        $this->password = $env['DB_PASS'];
+        $this->port = $env['DB_PORT'];
+    }
+
+    public function connect()
+    {
+        $this->conn = null;
+
+        try {
+            $dsn = "mysql:host={$this->host};port={$this->port};dbname={$this->db_name};charset=utf8";
+            $this->conn = new PDO($dsn, $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            echo "Conexión fallida: " . $e->getMessage();
+        }
+
+        return $this->conn;
+    }
+}
