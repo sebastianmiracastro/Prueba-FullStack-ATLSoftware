a78711badc5d482c4fa870acc948c6ab0b3fd18b
sebastianmiracastro <smira44@misena.edu.co>
2025-07-06 23:34:20 -0500

    Feature: se habilita los modulos de http para la lectura de archivos json, esto por la versión de Angular 18 sin modulos [20250706]

--- frontend/contacto/src/app/app.config.ts
+++ frontend/contacto/src/app/app.config.ts
@@ -1,9 +1,14 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection
+} from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(),
     provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    
+    provideZoneChangeDetection({ eventCoalescing: true })
   ]
 };
