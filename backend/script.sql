26f0eef3f004d6aa3c58b0006dcc7a401ea957dc
sebastianmiracastro <smira44@misena.edu.co>
2025-07-06 20:42:34 -0500

    Feature: se añade script sql para almacenado de datos [20250706]

---
+++ backend/script.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE prueba_atl_software;
+USE prueba_atl_software;
+
+CREATE TABLE contacts(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    email VARCHAR(150) NOT NULL UNIQUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE phones(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    contact_id INT NOT NULL,
+    phone_number VARCHAR(50) NOT NULL,
+    FOREIGN KEY (contact_id) REFERENCES contacts(id) ON DELETE CASCADE
+);
